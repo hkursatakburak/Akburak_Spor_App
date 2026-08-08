a59aeb4d9657f73361aab7f70a103fdb88ccc038
hkursatakburak <akburakkursat54@gmail.com>
2026-08-08 15:09:31 +0300

    chore: Rename package name to com.akburaksystems.akburakspor for Google Play Console compatibility

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,7 +15,7 @@ if (keystorePropertiesFile.exists()) {
 }
 
 android {
-    namespace = "com.example.akb_app"
+    namespace = "com.akburaksystems.akburakspor"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -30,7 +30,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.akb_app"
+        applicationId = "com.akburaksystems.akburakspor"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/akburaksystems/akburakspor/MainActivity.kt
+++ android/app/src/main/kotlin/com/akburaksystems/akburakspor/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.akb_app
+package com.akburaksystems.akburakspor
 
 import io.flutter.embedding.android.FlutterActivity
 
